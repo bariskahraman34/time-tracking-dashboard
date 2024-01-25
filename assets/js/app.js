843e536b94860e82dd30133318a6ad2674e72573
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-25 07:13:19 +0300

    Daily, Weekly ve Monthly butonlarına tıklandığında içeriklerin değişmesi olayı tamamlandı. Hover efektleri yapılacak.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 const dailyRoutines = document.querySelector('.daily-rutines-container');
 const titles = document.querySelectorAll('.title');
+const time = document.querySelectorAll('.time');
+const timezone = document.querySelectorAll('.timezone-btn');
+const previous = document.querySelectorAll('.last-week');
+
+let periodOfTime;
 
 const jsonFilePath = 'assets/js/data.json';
 
@@ -9,10 +14,38 @@ async function fetchData(jsonFilePath){
     return getData(data)
 }
 
+for (const period of timezone) {
+    period.addEventListener('click',bindPeriodBtn)
+}
+
+function bindPeriodBtn(){
+    for (const period of timezone) {
+        period.classList.remove('active');
+    }
+    this.classList.add('active');
+    periodOfTime = this.id;
+    console.log(periodOfTime)
+    fetchData(jsonFilePath);
+}
+
 async function getData(data){
-    console.log(data);
+    for (const period of timezone) {
+        if(period.classList.contains('active')){
+            periodOfTime = period.id;
+        }
+    }
     for (let i = 0 ; i < titles.length ; i++) {
-        titles[i].innerHTML = `${data[i].title}`
+        titles[i].innerHTML = `${data[i].title}`;
+        if(periodOfTime == "weekly"){
+            time[i].innerHTML = `${data[i].timeframes.weekly.current}hrs`
+            previous[i].innerHTML = `Last-Week ${data[i].timeframes.weekly.previous}hrs`
+        }else if(periodOfTime == "daily"){
+            time[i].innerHTML = `${data[i].timeframes.daily.current}hrs`
+            previous[i].innerHTML = `Last-Week ${data[i].timeframes.daily.previous}hrs`
+        }else if(periodOfTime == "monthly"){
+            time[i].innerHTML = `${data[i].timeframes.monthly.current}hrs`
+            previous[i].innerHTML = `Last-Week ${data[i].timeframes.monthly.previous}hrs`
+        }
     }
 }
 
