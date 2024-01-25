a027e907a0e0b00b2eaeeac052dc117694a780c2
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-25 06:45:36 +0300

    Çekilen data ile başlıkların  içine data dan gelen verinin yazdırılması işlemi yapıldı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,19 @@
+const dailyRoutines = document.querySelector('.daily-rutines-container');
+const titles = document.querySelectorAll('.title');
+
 const jsonFilePath = 'assets/js/data.json';
 
 async function fetchData(jsonFilePath){
     const response = await fetch(jsonFilePath)
     const data = await response.json();
-    getData(data)
+    return getData(data)
 }
 
 async function getData(data){
     console.log(data);
+    for (let i = 0 ; i < titles.length ; i++) {
+        titles[i].innerHTML = `${data[i].title}`
+    }
 }
 
 fetchData(jsonFilePath)
